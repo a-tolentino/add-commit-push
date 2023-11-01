7f9a145673eb643b70c098276c998513b3b89ad8
Andrew Tolentino <AndoyT2005@gmail.com>
2023-11-01 11:22:02 -0500

    Update files.

---
+++ add-commit-push.py
@@ -0,0 +1,12 @@
+import os
+
+print('Starting add-commit-push')
+print('\ngit status')
+os.system('git status')
+
+print('\ngit add -A')
+os.system('git add -A')
+print('\ngit commit -m "Update files."')
+os.system('git commit -m "Update files."')
+print('\ngit push')
+os.system('git push')
